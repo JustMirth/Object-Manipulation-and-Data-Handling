eaa49205bb804c82b80c385e246bd50a321dcdd3
Mitchell Callaghan <mitchy1898@gmail.com>
2024-11-27 08:30:50 -0500

    Removed Grade property and added console log to display required outputs

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const student = {
     name: "Mitchell Callaghan",
     age: 24,
     enrolled: true, 
-    courses: ["Operating Systems", "Fundamentals of JavaScript", "Webprogramming and Design", "Database Concepts and Design", "Algorithms and Problem Solving", "Technical Reading and Writing Skills"],
-    finalGrades: ["", "", "", "", "", ""],
+    courses: ["Operating Systems", "Fundamentals of JavaScript", "Web Programming and Design", "Database Concepts and Design", "Algorithms and Problem Solving", "Technical Reading and Writing Skills"],
     displayInfo: function() {
         return `Name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses}`;
     }
 };
 
-console.log(`Final Grade: ${student.finalGrades}`);
+console.log(`Name: ${student.name} and Age: ${student.age}`);
+console.log(student.displayInfo());
